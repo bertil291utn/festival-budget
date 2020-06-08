03480ca5ebf5d243db73fd06c26d4b45f2dfd10c
bertil291utn <bertilt4@gmail.com>
2020-06-08 11:54:53 -0500

    Session controller - Add routes for login and signup functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
   resources :users, only: %i[new create show]
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
 end
