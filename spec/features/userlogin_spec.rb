93f9d39422af325b0a1f43060993d5d889d3c7e6
bertil291utn <bertilt4@gmail.com>
2020-06-09 15:54:10 -0500

    Add log out integration testing

--- spec/features/userlogin_spec.rb
+++ spec/features/userlogin_spec.rb
@@ -8,4 +8,12 @@ RSpec.feature 'Userlogins', type: :feature do
     click_on 'Login'
     expect(page).to have_current_path root_path
   end
+
+  describe 'logout' do
+    scenario 'logout' do
+      visit '/logout'
+      expect(page).to have_current_path login_path
+    end
+    it { should_not reset_session! }
+  end
 end
