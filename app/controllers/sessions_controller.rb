4023f9c0de21c6823e81e4a85fa8625978965680
bertil291utn <bertilt4@gmail.com>
2020-06-09 12:34:01 -0500

    Remove class variables

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -2,9 +2,9 @@ class SessionsController < ApplicationController
   def new; end
 
   def create
-    @user = User.find_by_name(params[:username])
-    if @user
-      log_in @user
+    user = User.find_by_name(params[:username])
+    if user
+      log_in user
       redirect_to root_url, notice: 'Logged in!'
     else
       render :new, alert: 'Username is invalid'
