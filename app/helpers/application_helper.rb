f664bf40df2c691d334b5ed30b56054cd6d7927a
bertil291utn <bertilt4@gmail.com>
2020-06-13 21:33:41 -0500

    Ternary operator to change redirects - Add helper to solve current_path fucntion

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,5 @@
 module ApplicationHelper
+  def current_path(controlador, actionable = nil)
+    params[:controller] == controlador && (params[:action] == actionable unless actionable.nil?)
+  end
 end
