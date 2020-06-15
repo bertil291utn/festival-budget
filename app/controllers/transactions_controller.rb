833278bab4cb701a545e13a5135f281c48a260e7
bertil291utn <bertilt4@gmail.com>
2020-06-14 22:40:07 -0500

    Display with Bootstrap savings with no festival

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -1,8 +1,8 @@
-class TransactionsController < ApplicationController
-  @@current_page = ''
+# frozen_string_literal: true
 
+class TransactionsController < ApplicationController
   def index
-    @@current_page = 'festival'
+    @@current_path = 'festival'
     @transactions = User.find(current_user.id).transactions.with_festival
   end
 
@@ -19,10 +19,11 @@ class TransactionsController < ApplicationController
     @transaction = user.transactions.build(transaction_params)
     return render :new unless @transaction.save
 
-    redirect_to @@current_page.eql?('festival') ? transactions_path : no_festival_path, notice: 'Thanks for feeding the piglet'
+    redirect_to @@current_path.eql?('no_festival') ? no_festival_path : transactions_path, notice: 'Thanks for feeding the piglet'
   end
 
   def no_festival
+    @@current_path = 'no_festival'
     @transactions = User.find(current_user.id).transactions.without_festival
   end
 
