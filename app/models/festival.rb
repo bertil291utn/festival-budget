78091056857d6a36f8049455c88d96e313c0568f
bertil291utn <bertilt4@gmail.com>
2020-06-15 01:08:01 -0500

    Move upload image button at the beggining in sign up view - Change from username to ful name display author in festivals view

--- app/models/festival.rb
+++ app/models/festival.rb
@@ -1,3 +1,5 @@
+# frozen_string_literal: true
+
 class Festival < ApplicationRecord
   belongs_to :fest_creator, class_name: :User, foreign_key: :user_id
   has_many :transactions, dependent: :destroy
@@ -9,6 +11,6 @@ class Festival < ApplicationRecord
   validates :name, length: { minimum: 7 }
   validates :budget, format: { with: /\A\d{1,6}(\.\d{0,2})?\z/ }, numericality: { greater_than: 100 }
 
-  scope :with_username, -> { joins(:fest_creator).select('festivals.*, username') }
+  scope :with_username, -> { joins(:fest_creator).select('festivals.*, fullname') }
   scope :ordered, -> { order(name: :asc) }
 end
