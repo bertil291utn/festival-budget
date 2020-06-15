c5d145a44c019c4b2c751b67a892fcfa325e017d
bertil291utn <bertilt4@gmail.com>
2020-06-15 07:19:20 -0500

    Turn and off  rename columns - On development (sqlite3)

--- db/migrate/20200613162027_change_column_name.rb
+++ db/migrate/20200613162027_change_column_name.rb
@@ -1,12 +1,18 @@
+# frozen_string_literal: true
+
 class ChangeColumnName < ActiveRecord::Migration[5.2]
   def change
-    connection.execute('PRAGMA defer_foreign_keys = ON')
-    connection.execute('PRAGMA foreign_keys = OFF')
+    if Rails.env.development?
+      connection.execute('PRAGMA defer_foreign_keys = ON')
+      connection.execute('PRAGMA foreign_keys = OFF')
+    end
     rename_column :festivals, :image_file_name, :imageok_file_name
     rename_column :festivals, :image_content_type, :imageok_content_type
     rename_column :festivals, :image_file_size, :imageok_file_size
     rename_column :festivals, :image_updated_at, :imageok_updated_at
-    connection.execute('PRAGMA foreign_keys = ON')
-    connection.execute('PRAGMA defer_foreign_keys = OFF')
+    if Rails.env.development?
+      connection.execute('PRAGMA defer_foreign_keys = ON')
+      connection.execute('PRAGMA foreign_keys = OFF')
+    end
   end
 end
--- db/migrate/20200613193903_remove_image_from_festivals.rb
+++ db/migrate/20200613193903_remove_image_from_festivals.rb
@@ -1,9 +1,15 @@
+# frozen_string_literal: true
+
 class RemoveImageFromFestivals < ActiveRecord::Migration[5.2]
   def change
-    connection.execute('PRAGMA defer_foreign_keys = ON')
-    connection.execute('PRAGMA foreign_keys = OFF')
+    if Rails.env.development?
+      connection.execute('PRAGMA defer_foreign_keys = ON')
+      connection.execute('PRAGMA foreign_keys = OFF')
+    end
     remove_column :festivals, :image, :string
-    connection.execute('PRAGMA defer_foreign_keys = ON')
-    connection.execute('PRAGMA foreign_keys = OFF')
+    if Rails.env.development?
+      connection.execute('PRAGMA defer_foreign_keys = ON')
+      connection.execute('PRAGMA foreign_keys = OFF')
+    end
   end
 end
